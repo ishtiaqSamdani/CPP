e1d19424f26d434b0783fdd844e918c621354a18
ishtiaqSamdani <82057297+ishtiaqSamdani@users.noreply.github.com>
2022-06-06 21:12:11 +0530

    sorting is ip before using imp

--- inBuiltfunctionsVector/unique.cpp
+++ inBuiltfunctionsVector/unique.cpp
@@ -12,12 +12,14 @@ void showVec(vector<int> a)
 }
 int main()
 {
+    //sorting is important
     vector<int> ve = {1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 5, 5};
     unique(ve.begin(), ve.end()); // 1 2 3 4 5 3 3 3 4 4 4 5 5 5 5 5
 
-    vector<int> ve2 = {1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 5, 5};
+    vector<int> ve2 = {1,2,3,1};
+    sort(ve2.begin(),ve2.end());//sorting is important
     auto res = unique(ve2.begin(), ve2.end()); // res is the pointer pointing towards last unique value
-    cout << *res;                              // prints 3 no idea why
+    // cout << *res;                              // prints 3 no idea why
     vector<int> p = vector<int>(ve2.begin(), res);
     showVec(p);
 
